efd994237a36d070965b069e216f140b955e40a0
Daron Wiafe <daronkwiafe@gmail.com>
2022-10-26 08:03:48 +0200

    Replicate UI calculator

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react'; // eslint-disable-line
 import ReactDOM from 'react-dom/client'; // eslint-disable-line
 import './index.css';
-import App from './App'; // eslint-disable-line
-import reportWebVitals from './reportWebVitals'; // eslint-disable-line
+import CalculatorDom from './components/calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CalculatorDom />
   </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
